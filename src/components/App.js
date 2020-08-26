f155cdac2de10171bfb1a323c8e73aa7b6a655be
Ginny Mangat <mangatgrinpal@gmail.com>
2020-08-25 19:25:21 -0700

    changed file structure, started home component

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Home from './Home';
+import logo from '../logo.svg';
+import '../stylesheets/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Head 2 Head Basketball Stats
+      </header>
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+
+const Home = () => {
+
+	
+
+	return <h1>Hello world!</h1>
+}
+
+export default Home
